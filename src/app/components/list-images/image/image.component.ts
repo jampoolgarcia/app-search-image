71e48382fb5965bcca125e0c9a03fe9b20c9d765
jampoolG <jampoolgarcia@gmail.com>
2021-07-08 16:12:20 -0400

    added views of images list

--- src/app/components/list-images/image/image.component.ts
+++ src/app/components/list-images/image/image.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ImageI } from 'src/app/model/image';
 
 @Component({
   selector: 'app-image',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
     .h-45 {
       height: 45%;
     }
+
+    .h-px-350 {
+      height: 300px;
+    }
   `]
 })
 export class ImageComponent implements OnInit {
+
+  @Input() image!: ImageI;
 
   constructor() { }
 
