c7050653f1a9e144d3c3877663158f583154dcb5
jampoolG <jampoolgarcia@gmail.com>
2021-07-08 10:52:09 -0400

    added searchImage template

--- src/app/components/search-images/search-images.component.ts
+++ src/app/components/search-images/search-images.component.ts
@@ -3,8 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-images',
   templateUrl: './search-images.component.html',
-  styles: [
-  ]
+  styles: [`
+    .w-40 {
+      width: 40% !important;
+    }
+
+    .text-lg {
+      font-size: 2rem !important;
+    }
+
+    .lg {
+      width: 3rem; 
+      height: 3rem;
+    }
+
+    .b-left-0 {
+      border-top-left-radius: 0 !important;
+      border-bottom-left-radius: 0 !important;
+    }
+  `]
 })
 export class SearchImagesComponent implements OnInit {
 
